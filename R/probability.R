f2d100c65d5ff84b98c4fb8ffa56ebe540e6821b
C. Lente <7017340+clente@users.noreply.github.com>
2021-06-08 01:48:40 -0300

    Faster roll_outcome_count()

--- R/probability.R
+++ R/probability.R
@@ -3,7 +3,7 @@
 dice_outcome_count <- function(sides, n = 1) {
 
   # Create and init a matrix of long integers
-  mat <- purrr::map(1:(sides*n+1), ~as.list(VeryLargeIntegers::vli(sides*n+1)))
+  mat <- purrr::map(1:(sides*n+1), ~as.list(1:(sides*n+1)))
   mat[[1]][[1]] = VeryLargeIntegers::as.vli(1)
 
   # Calculate choose(i, j) dynamically
@@ -32,30 +32,40 @@ dice_outcome_count <- function(sides, n = 1) {
   }
 
   # Iterate over all possible values dice can add up to
-  purrr::map(n:(sides*n), ~list(outcome = .x, count = purrr::map_dbl(.x, sigma)))
+  n:(sides*n) %>%
+    purrr::map(~list(outcome = .x, count = purrr::map_dbl(.x, sigma)))
 }
 
-# Calculate the probability of each outcome of a dice formula
-dice_prob <- function(dice) {
-  dice %>%
+# Calculate the absolute frequency of each outcome of a dice formula
+roll_outcome_count <- function(roll) {
+
+  # Clean roll expression
+  roll <- stringr::str_remove_all(roll, "\\s")
+
+  # Create a function that takes dice's values and returns the roll's outcome
+  roll_function <- roll %>%
+    stringr::str_count(dice_regex) %>%
+    base::seq(1, .) %>%
+    purrr::reduce(~stringr::str_replace(.x, dice_regex, paste0("dots[[", .y, "]]")), .init = roll) %>%
+    stringr::str_c("function(...) { dots <- list(...); ", ., " }") %>%
+    rlang::parse_expr() %>%
+    rlang::eval_tidy()
+
+  # Combine the outcomes of each die and calculate the full outcome
+  roll %>%
     stringr::str_extract_all(dice_regex, simplify = TRUE) %>%
     purrr::map(stringr::str_extract_all, "[:digit:]+", simplify = TRUE) %>%
     purrr::map(~rev(as.numeric(.x))) %>%
     purrr::map(~purrr::exec(dice_outcome_count, !!!.x)) %>%
     purrr::cross() %>%
     purrr::map(purrr::transpose) %>%
-
     purrr::map(~list(
-      outcome = rlang::eval_tidy(rlang::parse_expr(
-        purrr::reduce(.x$outcome, ~stringr::str_replace(.x, dice_regex, as.character(.y)), .init = expr)
-      )),
-      count = purrr::reduce(.x$count, prod)
+      outcome = purrr::lift_dl(roll_function)(.x$outcome),
+      count = prod(as.numeric(.x$count))
     )) %>%
-
     purrr::transpose() %>%
     purrr::map(as.numeric) %>%
     dplyr::as_tibble() %>%
-
     dplyr::group_by(outcome) %>%
     dplyr::summarise(count = sum(count)) %>%
     dplyr::arrange(outcome)
