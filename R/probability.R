f6429aa6a1bba58ce34b030ea88162c85a1c337c
C. Lente <7017340+clente@users.noreply.github.com>
2021-06-22 02:30:51 -0300

    roll_outcome_count() working with expressions

--- R/probability.R
+++ R/probability.R
@@ -16,44 +16,121 @@ dice_outcome_count <- function(sides, n = 1) {
   )
 }
 
+# Check if expression belongs to the Dice S4 class
+is_dice <- function(expr, env) {
+  tryCatch(class(eval(expr, env)), error = function(e) "None") == "Dice"
+}
+
+# Get dice_outcome_count() of an expression
+get_doc <- function(expr, env, dice = FALSE) {
+
+  # Evaluate and get DOC (if expression is of the form N * dS)
+  if (dice) {
+
+    # Clone package env because original is locked
+    new_env <- new.env(parent = env)
+
+    # Create new environment with a `*` that returns a DOC
+    assign("*", methods::setMethod("*", c("numeric", "Dice"), function(e1, e2) {
+      dice_outcome_count(max(v(e2)), e1)
+    }), envir = new_env)
+
+    # Evaluate expression
+    return(eval(expr, new_env))
+  }
+
+  # Return DOC directly (if expression is a single die)
+  return(dice_outcome_count(max(v(eval(expr, env)))))
+}
+
+# Mask dice as inputs of a function (and get their dice_outcome_count())
+mask_dice <- function(expr_and_counts, env, dice = FALSE) {
+
+  # Separate parts of input
+  expr <- expr_and_counts[[1]]
+  counts <- expr_and_counts[[2]]
+
+  # Get dice_outcome_count() of expression
+  doc <- get_doc(expr, env, dice)
+
+  # Create a pseudo-UUID
+  id <- paste0(sample(letters[1:16], 36, TRUE), collapse = "")
+
+  # Transform expression into unique input and append DOC
+  expr <- bquote(dots[[.(id)]])
+  counts <- append(counts, stats::setNames(list(doc), id))
+
+  return(list(expr, counts))
+}
+
+# Mask dice of a roll as inputs of a function (and their dice_outcome_count())
+mask_roll <- function(expr_and_counts, env = parent.frame()) {
+
+  # Separate parts of input
+  expr <- expr_and_counts[[1]]
+  counts <- expr_and_counts[[2]]
+
+  # Mask dice from LHS of expression
+  if (is_dice(expr[[2]], env)) {
+    out <- mask_dice(list(expr[[2]], counts), env)
+    expr[[2]] <- out[[1]]; counts <- out[[2]]
+  }
+
+  # Skip parentheses and unary functions
+  if (length(expr) == 2) {
+    return(mask_roll(list(expr[[2]], counts), env))
+  }
+
+  # DFS to evaluate every branch of the expression
+  idx <- which(sapply(expr, typeof) == "language")
+
+  # Mask dice from RHS of expression
+  if (is_dice(expr[[3]], env)) {
+
+    # Handle expressions of the form N * dS
+    if (length(idx) == 0) {
+      return(mask_dice(list(expr, counts), env, TRUE))
+    }
+
+    # Otherwise simply mask the dice
+    out <- mask_dice(list(expr[[3]], counts), env)
+    expr[[3]] <- out[[1]]; counts <- out[[2]]
+  }
+
+  # Iterate over both branches of the expression
+  for (i in idx) {
+    out <- mask_roll(list(expr[[i]], counts), env)
+    expr[[i]] <- out[[1]]; counts <- out[[2]]
+  }
+
+  return(list(expr, counts))
+}
+
 #' Calculate the absolute frequency of each outcome of a roll formula
 #' @param roll Roll formula
+#' @param env Environment where to evaluate
 #' @export
-roll_outcome_count <- function(roll) {
+roll_outcome_count <- function(roll, env = parent.frame()) {
 
-  # Clean roll formula
-  roll <- gsub("\\s", "", roll)
-
-  # Extract dice from roll formula
-  roll_dice <- regmatches(roll, gregexpr(dice_regex, roll))[[1]]
-
-  # Create the body of roll_function(), taking dots as input
-  roll_body <- Reduce(
-    f = function(s, i) sub(dice_regex, paste0("dots[[", i, "]]"), s),
-    x = seq_along(roll_dice), init = roll
-  )
+  # Capture roll expression and mask dice objects
+  expr_and_counts <- mask_roll(list(substitute(roll), list()), env)
 
   # Create a function that inputs values in the roll formula (final outcome)
-  roll_function <- eval(parse(
-    text = paste("function(...) { dots <- list(...);", roll_body, "}")
-  ))
-
-  # Extract arguments for dice_outcome_count()
-  dice_args <- Map(
-    f = function(d) as.list(rev(as.numeric(d))),
-    regmatches(roll_dice, gregexpr("[0-9]+", roll_dice))
-  )
-
-  # Calculate dice outcomes and get all permutations
-  dice_perm <- cross(Map(function(a) do.call(dice_outcome_count, a), dice_args))
+  roll_function <- function(...) {
+    dots <- list(...)
+    eval(expr_and_counts[[1]], envir = list(dots))
+  }
 
   # Transpose list of dice outcomes
-  dice_out <- Map(function(l) do.call(Map, c(f = c, l)), dice_perm)
+  dice_out <- Map(
+    function(l) do.call(Map, c(f = list, l)),
+    cross(expr_and_counts[[2]])
+  )
 
   # Actually roll dice with roll_function() and multiply outcomes' counts
   roll_out <- Map(function(l) data.frame(
     outcome = do.call(roll_function, as.list(l$outcome)),
-    count = prod(l$count)
+    count = do.call(prod, l$count)
   ), dice_out)
 
   # Summarise table by outcome, adding counts
--- tests/testthat/test-probability.R
+++ tests/testthat/test-probability.R
@@ -1,7 +1,11 @@
 test_that("roll_outcome_count() works", {
 
-  wide <- roll_outcome_count("3*2d6 + d20 + 1d10 - 1 +    4")
-  long <- roll_outcome_count("40d6")
+  d6 <- Dice(1:6)
+  d20 <- Dice(1:20)
+  d10 <- Dice(1:10)
+
+  wide <- roll_outcome_count(3* (2*d6) + d20 + 1*d10 - 1 +    4)
+  long <- roll_outcome_count(40 * d6)
 
   expect_equal(nrow(wide), 59)
   expect_equal(nrow(long), 201)
