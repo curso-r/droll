9d20c6f2a64272b10f9e2c2f8d3929322edb2450
C. Lente <7017340+clente@users.noreply.github.com>
2021-06-26 01:00:21 -0300

    R 3.5 doesn't seem to like barplot() formulas

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,8 @@
 
 plot_outcome_count <- function(roll, ...) {
   counts <- roll_outcome_count_(substitute(roll), parent.frame())
-  graphics::barplot(count ~ outcome, data = counts, ...)
+  graphics::barplot(
+    counts$count, names.arg = counts$outcome,
+    xlab = "outcome", ylab = "count"
+  )
 }
